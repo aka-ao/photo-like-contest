444692fc5f130a63520524577883a28b166e165b
akahori-soma <akahori.soma@gmail.com>
2024-01-13 16:26:07 +0900

    first commit

---
+++ src/firebase.ts
@@ -0,0 +1,20 @@
+import firebase, { initializeApp } from 'firebase/app';
+import 'firebase/database';
+import { getDatabase } from 'firebase/database';
+import 'firebase/storage';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+const database = getDatabase(app);
+
+export { app, storage, database };
