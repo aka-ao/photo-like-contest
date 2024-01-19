18bb653214866ef295024dd05a2ddebf6a39298f
akahori-soma <akahori.soma@gmail.com>
2024-01-20 00:23:57 +0900

    Add favorite functionality to ListPage component

--- src/ListPage.tsx
+++ src/ListPage.tsx
@@ -5,25 +5,29 @@ import {
   ref,
   uploadBytes,
 } from "firebase/storage";
+import { ref as dbRef, get, push } from "firebase/database";
 import React, { useEffect, useRef, useState } from "react";
-import { storage } from "./firebase";
+import { database, storage } from "./firebase";
 
 import AddIcon from "@mui/icons-material/Add";
 import SendIcon from "@mui/icons-material/Send";
 import { Snackbar } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import "./ListPage.css"; // CSSをインポート
+import { set } from "firebase/database";
 
 const ListPage = () => {
   const [images, setImages] = useState<string[]>([]);
+  const [favorites, setFavorites] = useState<string[]>([]); // お気に入りの画像のURLを格納する配列
   const [isSnackbarOpen, setIsSnackbarOpen] = useState(false);
   const [isNoFileSnackbarOpen, setIsNoFileSnackbarOpen] = useState(false);
   const [errorState, setErrorState] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const uploadFormContainerRef = useRef<HTMLDivElement>(null);
 
   const fetchImages = async () => {
     try {
-      console.log(process.env.REACT_APP_FIREBASE_STORAGE_BUCKET)
       const storageRef = ref(storage, "images/");
       const result = await listAll(storageRef);
       const sorted = result.items.sort((a, b) => {
@@ -42,9 +46,37 @@ const ListPage = () => {
       setErrorMessage("画像を取得できませんでした。");
     }
   };
+  const fetchFavorites = async () => {
+    const userFavoritesRef = dbRef(database, "userFavorites/" + "test_user");
+    const fav: string[] = [];
+    await get(userFavoritesRef)
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          const data = snapshot.val();
+          for (const key in data) {
+            fav.push(data[key].url);
+          }
+        } else {
+          console.log("No data available");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    setFavorites(fav);
+  }
 
   useEffect(() => {
     fetchImages();
+    fetchFavorites();
+    if (uploadFormContainerRef.current) {
+    const marginBottom = `${uploadFormContainerRef.current.offsetHeight}px`;
+    const imageGrid = document.querySelector('.image-grid');
+    if (imageGrid) {
+      const imageGrid = document.querySelector('.image-grid') as HTMLElement;
+      imageGrid.style.marginBottom = marginBottom;
+    }
+  }
   }, []);
 
   const fileInputRef = useRef<HTMLInputElement>(null);
@@ -83,6 +115,38 @@ const ListPage = () => {
     }
   };
 
+  const handleFavoriteButtonClick = async(url: string, username: string) => {
+    if (!favorites.includes(url)) {
+      const userFavoritesRef = dbRef(database, "userFavorites/" + username);
+      push(userFavoritesRef, {
+        url: url,
+      });
+      await fetchFavorites();
+      console.log("お気に入りに追加");
+      return;
+    }
+    console.log("お気に入りから削除");
+    const userFavoritesRef = dbRef(database, "userFavorites/" + username);
+    await get(userFavoritesRef)
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          const data = snapshot.val();
+          for (const key in data) {
+            if (data[key].url === url) {
+              const deleteRef = dbRef(database, "userFavorites/" + username + "/" + key);
+              set(deleteRef, {});
+            }
+          }
+        } else {
+          console.log("No data available");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    await fetchFavorites(); // お気に入り削除後にお気に入りを再取得
+  };
+
   const handleSnackbarClose = () => {
     setIsSnackbarOpen(false);
     setIsNoFileSnackbarOpen(false);
@@ -93,13 +157,19 @@ const ListPage = () => {
   return (
     <div className="list-page">
       <div className="image-grid">
-        {images.map((url, index) => (
+        {images.map((image, index) => (
           <div key={index} className="image-item">
-            <img src={url} alt="" />
+            <img src={image} alt="" />
+            <IconButton
+              className="like-icon"
+              onClick={async() => handleFavoriteButtonClick(image, "test_user")}
+            >
+              <FavoriteIcon style={{ color: favorites.includes(image) ? 'red' : 'grey' }} />
+            </IconButton>
           </div>
         ))}
       </div>
-      <div className="upload-form-container">
+      <div className="upload-form-container" ref={uploadFormContainerRef}>
         <div className="upload-form">
           <form onSubmit={handleFormSubmit}>
             <input
--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -14,5 +15,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
-
-export { app, storage };
+const database = getDatabase(app);
+export { app, storage, database };
